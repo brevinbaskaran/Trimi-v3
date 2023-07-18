6ae898d8114549491cfce3ecf70b9c38bc8bc41b
brevinbaskaran <brevin.baskaran@outlook.com>
2023-07-18 15:48:39 -0400

    Add a page not found

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import Home from "./pages/Home";
 import Classes from "./pages/Classes";
 import ClassesFrench from "./pages_fr/Classes_fr";
 
+// PageNotFound component
+const PageNotFound = () => {
+  return <h1>404 - Page Not Found</h1>;
+};
+
 export default function App() {
   const [language, setLanguage] = useState("EN");
 
@@ -33,6 +38,8 @@ export default function App() {
             exact
             component={language === "FR" ? ClassesFrench : Classes}
           />
+          {/* Page Not Found */}
+          <Route component={PageNotFound} />
         </Switch>
       </Router>
     </div>
